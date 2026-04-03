4152831fb3bae8e9f9e6915938f39e680ec21a5d
pvmuralikrishna481141-gif <pvmuralikrishna481141.com>
2026-04-03 10:03:09 +0530

    Initial commit: Stackpilot backend

---
+++ config/db.js
@@ -0,0 +1,14 @@
+// Database configuration
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
